33d46deaac1901a1d86c43750c9aa219f5001675
Ranyeri Klennes <46034451+Ranyeri-Klennes@users.noreply.github.com>
2021-06-22 20:10:45 +0000

    refrescando a memória com Java Orientado a Objetos.

--- Main.java
+++ Main.java
@@ -1,31 +1,9 @@
-class Main {
-	public class Caneta{
-		public String modelo;
-		public String cor;
-		private float ponta;
-		protected int carga;
-		protected boolean tampada;
-
-		void status(){
-			System.out.println("Modelo: "+ this.modelo);
-			System.out.println("Uma Caneta "+this.cor);
-			System.out.println("Ponta: "+this.ponta);
-			System.out.println("Carga: "+this.carga);
-			System.out.println("Está tampada? "+this.tampada);
-		}
-
-		void rabiscar() {
-			if (this.tampada == true) {
-				System.out.println("ERRO! Não posso rabiscar");
-			} else {
-				System.out.println("Estou rabiscando..");
-			}
-		}
-		void tampar(){
-			this.tampada = true;
-		}
-		void destampar(){
-			this.tampada = false;
-		}
+public class Main {
+	public static void main(String[] args){
+		Caneta c1 = new Caneta();
+		c1.cor = "Azul";
+		c1.ponta = 0.5f;
+		c1.tampada = false;
+		c1.status();
 	}
 }
